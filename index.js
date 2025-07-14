bfd4bd3b922a1ab82e1cd5f5a36845eef8cc5a4f
Dilnavazsida <navazdil07@gmail.com>
2025-07-14 11:01:17 +0530

    create first node program

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+// program 1
+
+// let user  = 7
+
+// if (user % 2 == 0){
+//     console.log("number is even")
+// }
+// else{
+//     console.log("number is odd")
+// }
+
+
+// program 2
+
+

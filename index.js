08d874c9c68c3ab5c74e99c592cdd0e05c1ee3c8
Dilnavazsida <navazdil07@gmail.com>
2025-07-17 09:17:48 +0530

    i add last program of this file

--- index.js
+++ index.js
@@ -308,3 +308,11 @@
 
                                     // program 20
 
+// function power(a,b){
+//             let result = 1 
+//             for(let i=1;i<=b;i++){
+//                 result *=a
+//             }
+//             return result
+// }
+// console.log(power(2,3))

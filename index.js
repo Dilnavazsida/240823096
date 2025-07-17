f4ab87583252505979bc80f632e3d4c5fbd9f56a
Dilnavazsida <navazdil07@gmail.com>
2025-07-17 09:04:53 +0530

    add 5 more program

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-// program 1
+                                    // program 1
 
 // let user  = 7
 
@@ -12,7 +12,7 @@
 // }
 
 
-// program 2
+                                    // program 2
 
 // let a = 5
 // let b = 9
@@ -26,7 +26,7 @@
 
 
 
-// program 3
+                                    // program 3
 
 // function isLeapYear(year) {
 //     if (year % 4 === 0) {
@@ -53,7 +53,7 @@
 
 
 
-//  program 4
+                                //  program 4
 
 
 // let user = 5 ;
@@ -66,7 +66,7 @@
 // console.log(output);
 
 
-//  program 5
+                                //  program 5
 
 // let user = 4
 
@@ -78,7 +78,7 @@
 // console.log("factorial is : ",fac)
 
 
-// program 6
+                                // program 6
 
 // let user = 5
 
@@ -87,7 +87,7 @@
 // }
 
 
-// program 7
+                                // program 7
 
 // let num = 1234;
 
@@ -96,7 +96,7 @@
 // console.log(reversed);
 
 
-// program 8
+                                // program 8
 
 
 // let user = 121
@@ -109,7 +109,7 @@
 // }
 
 
-// program 9
+                                // program 9
 
 // let user = 7
 // let isPrime = true
@@ -134,7 +134,7 @@
 
 
 
-// program 10
+                                    // program 10
 
 // let user = 123456
 // let len = 0
@@ -146,7 +146,7 @@
 // console.log(len)
 
 
-// program 11
+                                    // program 11
 
 // let num = 123
 // let toString = String(num)
@@ -157,7 +157,7 @@
 // console.log(sum)
 
 
-//  program 12
+                                //  program 12
 
 
 // function check_armstrong(num){
@@ -174,7 +174,7 @@
 // console.log(check_armstrong(num))
 
 
-//  program 13
+                                    //  program 13
 
 // function fibonacci(n){
 //         prevNum = 0
@@ -191,7 +191,7 @@
 // console.log(fibonacci(user))
 
 
-// program 14
+                                // program 14
 
 // function check_vovel(n){
 //         let vl = ['a','e','i','o','u','A','E','I','O','U']
@@ -204,3 +204,107 @@
 // }
 // let n = "a"
 // console.log(check_vovel(n))
+
+
+                                // program 15
+
+// let input = "4 + 3"
+
+// let parts = input.split(" ")
+// let num1 = parseFloat(parts[0])
+// let operator = parts[1]
+// let num2 = parseFloat(parts[2])
+
+// let result;
+
+// switch (operator){
+//     case "+":
+//         result = num1 + num2
+//         break
+//     case "-":
+//         result = num1 - num2
+//         break
+//      case "*":
+//         result = num1 * num2
+//         break
+//     case "/":
+//         result =  num2 !== 0 ? num1/num2 : "can not devide by zero"
+//         break
+//     default:
+//         result = "invalid input"
+// }
+// console.log("output",result)
+
+
+                        //  program 16
+
+// function find_gcd(a,b){
+//    while(b !== 0){ 
+//         let temp = b
+//         b = a % b
+//         a = temp
+//    }
+//    return a
+// }
+
+// let num1 = 20
+// let num2 = 28
+
+// let result = find_gcd(num1,num2)
+// console.log(result)
+
+
+
+                        //  program 17
+
+// function check_perfect_num(a){
+//     sum = 0
+//     for(let i =1; i<a; i++){
+//         if (a % i == 0){
+//             sum += i
+//         }
+//     }
+//      if (sum == a){
+//         return "Perfect"
+//     }
+//     else{
+//         return "not Perfect"
+//     }
+// }
+// console.log(check_perfect_num(28))
+
+
+
+                                    // program 18
+
+// function dis_all_div_int(a){
+//     all = ""
+
+//     for(let i=1;i<=a;i++){
+//         if(a % i == 0){
+//                 all += String(i)
+//         }
+//     }
+//     return all
+// }
+// console.log(dis_all_div_int(10))
+
+
+                                    // program 19
+
+// function check_pos_neg_zero(num){
+//             if(num < 0){
+//                 return "negative"
+//             }
+//             else if(num == 0){
+//                 return "Zero"
+//             }
+//             else {
+//                 return "Positive"
+//             }
+// }
+// console.log(check_pos_neg_zero(1))
+
+
+                                    // program 20
+

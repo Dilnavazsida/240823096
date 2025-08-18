40bc02397e1e5ea6840489624af96832adb40a99
Dilnavazsida <navazdil07@gmail.com>
2025-08-18 11:37:48 +0530

    i add validations

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,3 +1,11 @@
 exports.index = (req,res)=>{
     res.send("Hello Mca Student")
 }
+
+exports.show = (req,res) =>{
+    res.send(`showing data for id : ${req.params.id}`)
+}
+
+exports.store = (req,res)=>{
+    res.send(`data received : ${req.body.name}, ${req.body.city}`)
+}

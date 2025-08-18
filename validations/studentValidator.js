a18da80bcc9375f4f716e67f2e527d4d525b4629
Dilnavazsida <navazdil07@gmail.com>
2025-08-18 11:31:27 +0530

    i add validations

---
+++ validations/studentValidator.js
@@ -0,0 +1,25 @@
+const joi = require("joi")
+
+exports.createValidator = joi.object({
+    name: joi.string().min(3).max(30).required(),
+    city: joi.string().min(3).max(50).required()
+});
+exports.updateValidator = joi.object({
+    name: joi.string().min(3).max(30).optional(),
+    city: joi.string().min(3).max(50).optional()
+});
+
+
+exports.validate = (schema, data) => {
+  const { error, value } = schema.validate(data, { abortEarly: false });
+  if (error) {
+    return {
+      success: false,
+      errors: error.details.map((err) => ({
+        field: err.path[0],
+        message: err.message,
+      })),
+    };
+  }
+  return { success: true, value };
+};
